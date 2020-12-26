dd8abda057368de708c14ec4b59dcc0f1e901def
honggaruy <honggaruy@gmail.com>
2020-12-26 23:24:26 +0900

    :umbrella: (test) Add profit test case
    * profit 테스트 케이스를 추가
    * 필요없는 주석코드를 제거
    * Province( ) 생성 부분에 중복이 있지만 과정을 남기기 위해 그대로 둠
    * @TODO : 다음 커밋에서 중복코드 제거할 것

--- chap04/test/test.js
+++ chap04/test/test.js
@@ -4,7 +4,10 @@ import {Province, sampleProvinceData} from '../src/main.js'
 describe('province', function() {
     it('shortfall', function() {
         const asia = new Province(sampleProvinceData())
-        //chai.assert.equal(asia.shortfall, 5)
         chai.expect(asia.shortfall).equal(5)
+    })
+    it('profit', function() {
+        const asia = new Province(sampleProvinceData())
+        chai.expect(asia.profit).equal(230)
     })
 })
