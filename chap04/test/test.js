d25d59506c268c64dd0202b50d416dfed4798b27
honggaruy <honggaruy@gmail.com>
2020-12-26 23:50:00 +0900

    :umbrella: (test) Avoid repeated fixture creation
    * mocha에서 제공하는 beforeEach() hook을 사용하여 중복을 제거
    * 그냥 바깥으로 끌어내면 fixture를 공유하게 되어 문제가 발생할 수도 있음

--- chap04/test/test.js
+++ chap04/test/test.js
@@ -2,12 +2,14 @@ import chai from 'chai'
 import {Province, sampleProvinceData} from '../src/main.js'
 
 describe('province', function() {
+    let asia;
+    beforeEach( function() {
+        asia = new Province(sampleProvinceData())
+    })
     it('shortfall', function() {
-        const asia = new Province(sampleProvinceData())
         chai.expect(asia.shortfall).equal(5)
     })
     it('profit', function() {
-        const asia = new Province(sampleProvinceData())
         chai.expect(asia.profit).equal(230)
     })
 })
