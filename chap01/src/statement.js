d87d1e4a0632db6242f2b8246242fa3d058d5dc4
honggaruy <honggaruy@gmail.com>
2020-12-28 00:00:59 +0900

    :hammer: (main) Refactor amount calcuration
    * 첫번째 refactoring
    * switch문으로 공연 요금 계산하는 부분을 refactor
    * `slide statements`(문장 슬라이드 하기) 이후
      * VS Code의 `Extract inner function in function 'xxx'` 기능을 사용

--- chap01/src/statement.js
+++ chap01/src/statement.js
@@ -8,25 +8,8 @@ export function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID]
-        let thisAmount = 0
 
-        switch (play.type) {
-            case "tragedy": // 비극
-                thisAmount = 40000
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30)
-                }
-                break
-            case "comedy": // 희극
-                thisAmount = 30000
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20) 
-                }
-                thisAmount += 300 * perf.audience
-                break
-            default:
-                throw new Error(`알 수 없는 장르: ${play.type}`)
-        }
+        let thisAmount = amountFor(play, perf);
 
         // 포인트를적립한다
         volumeCredits += Math.max(perf.audience - 30, 0)
@@ -40,6 +23,28 @@ export function statement(invoice, plays) {
     result += `총액: ${format(totalAmount/100)}\n`
     result += `적립 포인트: ${volumeCredits}점\n`
     return result
+
+    function amountFor(play, perf) {
+        let thisAmount = 0;
+        switch (play.type) {
+            case "tragedy": // 비극
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy": // 희극
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`);
+        }
+        return thisAmount;
+    }
 }
 
 import fs from 'fs'
