14d58bb21974321fd4adf41abd1eb28411b684a2
honggaruy <honggaruy@gmail.com>
2020-12-28 18:46:34 +0900

    :hammer: (1.6) render text 추출 - step7
    * 중간 데이터 구축 진행
    *  step6의 반복
    * playFor() 대신 amountFor()에 대해 진행

--- chap01/src/statement.js
+++ chap01/src/statement.js
@@ -7,6 +7,7 @@ export function statement(invoice, plays) {
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance)
         result.play = playFor(aPerformance)
+        result.amount = amountFor(result)
         return result
     }
 
@@ -14,12 +15,34 @@ export function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
+    function amountFor(aPerformance) {
+        let result = 0;
+        switch (aPerformance.play.type) {
+            case "tragedy": // 비극
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy": // 희극
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+        }
+        return result;
+    }
+
 }
 
 function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`
     for (let perf of data.performances) {
-        result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
+        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`
     }
 
     result += `총액: ${usd(totalAmount())}\n`
@@ -29,7 +52,7 @@ function renderPlainText(data, plays) {
     function totalAmount() {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
         return result;
     }
@@ -58,28 +81,6 @@ function renderPlainText(data, plays) {
         return result
     }
 
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy": // 희극
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-        }
-        return result;
-    }
 }
 
 import fs from 'fs'
