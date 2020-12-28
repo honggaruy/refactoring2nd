57202e3a4cdd6cffda475664d747d1dc3cf03793
honggaruy <honggaruy@gmail.com>
2020-12-28 12:54:59 +0900

    :hammer: (1.4 end) totalAmount  제거 - 1단계
    * scope : 1.4절 마지막 부분 내용 적용
    * 바로 앞의 volumenCredits과 유사함
     * Split loop, Slide statements 사용
    *  특이 사항 : totalAmount 이름이 선점되어 있으므로 임시이름사용

--- chap01/src/statement.js
+++ chap01/src/statement.js
@@ -1,16 +1,22 @@
 export function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`
-
     for (let perf of invoice.performances) {
-        // 청구내역을 출력한다
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
-        totalAmount += amountFor(perf)
     }
+
+    let totalAmount = appleSauce();
 
     result += `총액: ${usd(totalAmount)}\n`
     result += `적립 포인트: ${totalVolumeCredits()}점\n`
     return result
+
+    function appleSauce() {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
+    }
 
     function totalVolumeCredits() {
         let volumeCredits = 0;
