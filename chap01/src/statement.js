a5c5f15555c12019553de084deb98b62a338d9e3
honggaruy <honggaruy@gmail.com>
2020-12-28 20:22:53 +0900

    :hammer: (1.6) render text 추출 - step11
    * 데이터 처리에 해당하는 부분을 statement()와 분리
    * statement()는 render...()만 호출
    * 데이터처리 함수는 data를 반환하는 것으로 수정
    * reder...()는 필요없는 plays 매개변수 제거

--- chap01/src/statement.js
+++ chap01/src/statement.js
@@ -1,10 +1,14 @@
 export function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays))
+}
+
+function createStatementData(invoice, plays) {
     const statementData = {}
     statementData.customer = invoice.customer
     statementData.performances = invoice.performances.map(enrichPerformance)
     statementData.totalAmount = totalAmount(statementData)
     statementData.totalVolumeCredits = totalVolumeCredits(statementData)
-    return renderPlainText(statementData, plays)
+    return statementData
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance)
@@ -60,7 +64,7 @@ export function statement(invoice, plays) {
 
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `청구 내역 (고객명: ${data.customer})\n`
     for (let perf of data.performances) {
         result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`
