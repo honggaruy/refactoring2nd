2dd545a04943f9240cfc2c7fa38fd9ff40297749
honggaruy <honggaruy@gmail.com>
2020-12-28 18:36:28 +0900

    :hammer: (1.6) render text 추출 - step6
    * 중간 데이터 구축 시작
    *  playFor() 이동 : renderText() -> statement()
      * 각 공연당 해당 연극정보도 집어넣음
      * 적용 method:  8.1 함수 옮기기, Move Function
      * Rename Symbol을 사용하기 까다로움. 수동 편집

--- chap01/src/statement.js
+++ chap01/src/statement.js
@@ -6,14 +6,20 @@ export function statement(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance)
+        result.play = playFor(aPerformance)
         return result
     }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
 }
 
 function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`
     for (let perf of data.performances) {
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
+        result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     }
 
     result += `총액: ${usd(totalAmount())}\n`
@@ -47,18 +53,15 @@ function renderPlainText(data, plays) {
     function volumeCreditsFor(perf) {
         let result = 0
         result += Math.max(perf.audience - 30, 0);
-        if ("comedy" === playFor(perf).type)
+        if ("comedy" === perf.play.type)
             result += Math.floor(perf.audience / 5);
         return result
     }
 
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
 
     function amountFor(aPerformance) {
         let result = 0;
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy": // 비극
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -73,7 +76,7 @@ function renderPlainText(data, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
         }
         return result;
     }
